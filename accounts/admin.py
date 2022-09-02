c91713bc427431b402a4a86055528b952e3e4194
이민석 <dat0802@gmail.com>
2022-09-02 11:43:40 +0900

    :sparkles: (leeminseok8/feature/accounts/modeling) 계정 모델 admin 등록

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User, TeamGroup, History
 
 # Register your models here.
+
+@admin.register(User)
+class AdminPostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TeamGroup)
+class FreePostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(History)
+class NoticePostAdmin(admin.ModelAdmin):
+    pass
