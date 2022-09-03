9c1607dc67da6f1532c9302d39c30981b280e784
dev-choee <rhksgh99@gmail.com>
2022-09-04 01:01:04 +0900

    :memo: (rhksghsss/feature/boards/notice-board-api) 공지사항 viewset 작성

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .serializers import NoticePostSerializer
+from .models import NoticePost
 
 
 def board_app_home(request):
     return HttpResponse("Board app")
+
+class NoticePostViewSet(viewsets.ModelViewSet):
+    queryset = NoticePost.objects.all()
+    serializer_class = NoticePostSerializer
