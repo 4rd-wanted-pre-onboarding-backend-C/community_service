688c4250f06fcc17d63726cb6049cb3624777f78
이민석 <dat0802@gmail.com>
2022-09-05 04:08:36 +0900

    :sparkles: (leeminseok8/feature/accounts/login-api) 로그인 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,34 @@
 from django.http import HttpResponse
 
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from .serializers import SignInSerializer
 
 def accounts_app_home(request):
     return HttpResponse("Accounts app")
+
+
+class SignInView(APIView):
+
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = SignInSerializer(data=request.data)
+
+        if serializer.is_valid():
+            token = serializer.validated_data
+            return Response(
+                {
+                    "message": "로그인 되었습니다.",
+                    "access_token": token["access"],
+                    "refresh_token": token["refresh"],
+                },
+                status=status.HTTP_200_OK,
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
