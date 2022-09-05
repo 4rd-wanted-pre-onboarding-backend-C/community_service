5dc49ef4342e7a7f0b7ac839d81ecadc5920fe90
dev-choee <rhksgh99@gmail.com>
2022-09-05 09:30:34 +0900

    :recycle: (rhksghsss/feature/notice-board-api) 공지사항 serializers 칼럼 수정, views 로그인한 사용자 작성자로 저장 추가

--- boards/serializers.py
+++ boards/serializers.py
@@ -1,8 +1,20 @@
-from dataclasses import field
 from rest_framework import serializers
 from .models import NoticePost
 
 class NoticePostSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", input_formats=["%Y-%m-%d T%H:%M:%S", "%Y-%m-%d"],
+                                           read_only=True)
+    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", input_formats=["%Y-%m-%d T%H:%M:%S", "%Y-%m-%d"],
+                                           read_only=True)
+    author_username = serializers.SlugRelatedField(read_only=True, slug_field='username', source="author")
+
+
     class Meta:
         model = NoticePost
-        fields = ('author','title','content','is_fixed')
+        fields = ["id",
+                "created_at",
+                "updated_at",
+                "title",
+                "content",
+                "is_fixed",
+                "author_username"]
--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
 from .serializers import NoticePostSerializer
 from .models import NoticePost
 
@@ -7,6 +7,10 @@ from .models import NoticePost
 def board_app_home(request):
     return HttpResponse("Board app")
 
-class NoticePostViewSet(viewsets.ModelViewSet):
+class NoticePostViewSet(ModelViewSet):
     queryset = NoticePost.objects.all()
     serializer_class = NoticePostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author_id=self.request.user.id)
+        return super().perform_create(serializer)
