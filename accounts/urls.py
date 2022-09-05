872b40718487f540f63d22b190a11f0c532b631d
이민석 <dat0802@gmail.com>
2022-09-05 11:52:56 +0900

    :recycle: (leeminseok8/develop) 로그인 urls 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import RegisterAPIView, DeleteUserView
+from .views import RegisterAPIView, DeleteUserView, SignInView
 
 urlpatterns = [
     path("", views.accounts_app_home),
     path("register", RegisterAPIView.as_view()),
     path("delete/<int:pk>", DeleteUserView.as_view()),
+    path("login", SignInView.as_view())
 ]
